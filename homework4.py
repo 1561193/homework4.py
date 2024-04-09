e157d8266f5a22df021e0df1458cb51190bcb04e
1561193 <164223652+1561193@users.noreply.github.com>
2024-04-09 19:57:01 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+immutable_var = (1, 2, False, "book")
+print(immutable_var)  # Кортеж-неизменяемый список
+mutable_list = [50, 3, 18]
+print(mutable_list)
+print(mutable_list[2])
+mutable_list[2] = "string"
+print(mutable_list)
+
+
+
